adc35ab03bbf76fd54f2d06a81f228e6382d1f0e
Thiago Rovina <thiagorovina222@gmail.com>
2025-07-19 09:26:37 -0300

    Update index.php

--- trabalho-web/index.php
+++ trabalho-web/index.php
@@ -23,6 +23,8 @@
         </form>
     </div>
     <?php 
+        error_reporting(E_ALL); 
+        ini_set('display_errors', 1);
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $email = $_POST['email'] ?? '';
             $senha = $_POST['senha'] ?? '';
