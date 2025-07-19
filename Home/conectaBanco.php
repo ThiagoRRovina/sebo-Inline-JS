9e6689528ef6915b4b71265d8cf9279eeec94c93
Thiago Rovina <thiagorovina222@gmail.com>
2025-07-19 08:50:39 -0300

    Delete Home directory

--- Home/conectaBanco.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conn = pg_connect("host=localhost port=5433 dbname=postgres user=postgres password=12345");
-
-if (!$conn) {
-    die("Conexão falhou: " . pg_last_error());
-} else {
-    echo "Conexão bem-sucedida!";
-}
-?>
