5a01e24e9f84db807c3ac99fd3a3b17e96273406
Thiago Rovina <thiagorovina222@gmail.com>
2025-07-19 10:08:19 -0300

    Update conectaBanco.php

--- trabalho-web/conectaBanco.php
+++ trabalho-web/conectaBanco.php
@@ -1,16 +1,7 @@
 <?php
-
-error_reporting(E_ALL); 
-ini_set('display_errors', 1);
-$host = getenv('PG_HOST');
-$port = getenv('PG_PORT');
-$dbname = getenv('PG_DATABASE');
-$user = getenv('PG_USER');
-$password = getenv('PG_PASSWORD');
-
-$conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
+$conn = pg_connect("host=localhost port=5433 dbname=postgres user=postgress password=12345");
 
 if (!$conn) {
     die("Conexão falhou: " . pg_last_error());
-}
+} 
 ?>
